5cd140bf94d45e84d569f3efd4a53c8632fb62e7
youssef khalifi <youssefkhalifi767@gmail.com>
2024-05-01 17:39:37 +0100

    creation de projet

---
+++ src/main/java/com/gridcrud/GridCrudApplication.java
@@ -0,0 +1,13 @@
+package com.gridcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gridcrud/GridCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
